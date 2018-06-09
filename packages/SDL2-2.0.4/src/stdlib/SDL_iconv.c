a99574266746839ff90509093847c0923e6fee3b
sezero <sezero@users.sourceforge.net>
2018-06-10 02:32:24 +0300

    removed changed added to stdlib/SDL_iconv.c by the os/2 port.

--- packages/SDL2-2.0.4/src/stdlib/SDL_iconv.c
+++ packages/SDL2-2.0.4/src/stdlib/SDL_iconv.c
@@ -36,7 +36,7 @@
    iconv() may or may not use const char ** for the inbuf param.
    If we get this wrong, it's just a warning, so no big deal.
 */
-#if defined(_XGP6) || defined(__APPLE__) || defined(__WATCOMC__) || \
+#if defined(_XGP6) || defined(__APPLE__) || \
     (defined(__GLIBC__) && ((__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 2)) || \
     (defined(_NEWLIB_VERSION)))
 #define ICONV_INBUF_NONCONST
