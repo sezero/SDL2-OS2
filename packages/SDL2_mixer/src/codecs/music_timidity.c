4dc65c69040e3ee0e934cf4a446c11c3f6b045c1
Ozkan Sezer <sezeroz@gmail.com>
2021-03-20 03:11:21 +0300

    updates to SDL_mixer and SDL_sound from mainstream.

--- packages/SDL2_mixer/src/codecs/music_timidity.c
+++ packages/SDL2_mixer/src/codecs/music_timidity.c
@@ -152,6 +152,12 @@ static int TIMIDITY_Play(void *context, int play_count)
     return TIMIDITY_Seek(music, 0.0);
 }
 
+static SDL_bool TIMIDITY_IsPlaying(void *context)
+{
+    TIMIDITY_Music *music = (TIMIDITY_Music *)context;
+    return Timidity_IsActive(music->song);
+}
+
 static int TIMIDITY_GetSome(void *context, void *data, int bytes, SDL_bool *done)
 {
     TIMIDITY_Music *music = (TIMIDITY_Music *)context;
@@ -216,6 +222,12 @@ static int TIMIDITY_Seek(void *context, double position)
     return 0;
 }
 
+static double TIMIDITY_Tell(void *context)
+{
+    TIMIDITY_Music *music = (TIMIDITY_Music *)context;
+    return Timidity_GetSongTime(music->song) / 1000.0;
+}
+
 static double TIMIDITY_Duration(void *context)
 {
     TIMIDITY_Music *music = (TIMIDITY_Music *)context;
@@ -253,11 +265,11 @@ Mix_MusicInterface Mix_MusicInterface_TIMIDITY =
     TIMIDITY_SetVolume,
     TIMIDITY_GetVolume,
     TIMIDITY_Play,
-    NULL,   /* IsPlaying */
+    TIMIDITY_IsPlaying,
     TIMIDITY_GetAudio,
     NULL,   /* Jump */
     TIMIDITY_Seek,
-    NULL,   /* Tell */
+    TIMIDITY_Tell,
     TIMIDITY_Duration,
     NULL,   /* LoopStart */
     NULL,   /* LoopEnd */
