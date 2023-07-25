cfc3c1c3b1fcc93370ba4f3f03c5d74ffc8bdc8a
Ozkan Sezer <sezeroz@gmail.com>
2023-07-25 14:25:56 +0300

    updates to SDL2 from mainstream

--- packages/SDL2/include/SDL_revision.h
+++ packages/SDL2/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/sezero/SDL.git@14a6c73d59fc96c9b4b66cd1a27d536512199d96"
+#define SDL_REVISION "https://github.com/sezero/SDL.git@39905eaa1bfb941f3370f38eaa7c2d3ecfc9391b"
 #define SDL_REVISION_NUMBER 0
