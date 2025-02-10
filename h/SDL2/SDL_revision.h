db3660f026ea37b1876d4587b97db0d92958a041
Ozkan Sezer <sezeroz@gmail.com>
2025-02-10 05:27:46 +0300

    updates from mainstream.

--- h/SDL2/SDL_revision.h
+++ h/SDL2/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/sezero/SDL.git@1334bce1362e082a0689b545f176da504e29a33c"
+#define SDL_REVISION "https://github.com/sezero/SDL.git@bd1d5b564c1463303b4edf41acfe6f5c0ccfe74d"
 #define SDL_REVISION_NUMBER 0
